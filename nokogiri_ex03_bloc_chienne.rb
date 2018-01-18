c4379e51d401395d85ae133139dc80c1d5bee500
bseb <bailly93@gmail.com>
2018-01-19 00:51:14 +0100

    final

--- nokogiri_ex03_bloc_chienne.rb
+++ nokogiri_ex03_bloc_chienne.rb
@@ -7,19 +7,18 @@ def crypto_currency
 	page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 	stock_prices = page.xpath('//td[5]/a')
 	currency = page.xpath('//td[2]/a')
-	array_tock_prices = []
-	
+	array_stock_prices = []
 	boucle_infini = 0
-		while (boucle_infini)
-			hash_stock = Hash.new
-			stock_prices[0..1500].each do |temp| # <= grandes boucles pour récuperer le plus grand nombre de nonnaie.
-				hash_stock[currency[stock_prices.index(temp)].text] = temp.text
-				array_tock_prices << hash_stock
-			end
+	while (boucle_infini < 20)
+		hash_stock = Hash.new
+		stock_prices[0..1500].each do |temp| # <= grandes boucles pour récuperer le plus grand nombre de nonnaie.
+		hash_stock[currency[stock_prices.index(temp)].text] = temp.text
+		array_stock_prices << hash_stock
+		end
 
-		i = array_tock_prices.length
-		puts array_tock_prices[i - 1]
-		sleep(3600) # <= timer sur 3600 seconde(1 heure)
+		i = array_stock_prices.length
+		puts array_stock_prices[i - 1]
+		sleep(5) # <= timer sur 3600 seconde(1 heure)
 		puts " "
 		puts " "
 		puts " "
@@ -27,8 +26,7 @@ def crypto_currency
 		puts " "
 	    puts " "
 		puts " "
-
-		end
+	end
 
 end
 puts crypto_currency
