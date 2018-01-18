ff9a6d094d42c62310a69e56954e863556fa72e5
bseb <bailly93@gmail.com>
2018-01-18 19:17:44 +0100

    2er commit

--- ex02_recup_url_ville/nokogiri_ex02_recup_url_ville.rb
+++ ex02_recup_url_ville/nokogiri_ex02_recup_url_ville.rb
@@ -5,24 +5,26 @@ require 'open-uri'
 def get_the_email_of_a_townhal_from_its_webpage(url)
 	page = Nokogiri::HTML(open(url))
 
-	@email_adress = ""
+	#@email_adress = ""
 	page.css("tr > td > p > font").each do |email|
 		if email.text.include?"@"
-			@array_link << email.text
+			return email.text
+			#@array_link << email.text
 		end
 	end
-	puts @array_link
+	#puts @array_link
 end
 
 def get_all_the_urls_of_val_doise_townhalls
+	hash_mairie = Hash.new
 	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
 	@array_link = []
 	list_url = page.css("a.lientxt")
 	list_url.each do |url|
 		good_link = "http://annuaire-des-mairies.com" + url['href'][1..url['href'].length]
-		get_the_email_of_a_townhal_from_its_webpage(good_link)
-		@array_link << good_link
+		hash_mairie[url.text] = get_the_email_of_a_townhal_from_its_webpage(good_link)
+		#@array_link << good_link
 	end
-	return @array_link
+	return hash_mairie
 end
 puts get_all_the_urls_of_val_doise_townhalls
